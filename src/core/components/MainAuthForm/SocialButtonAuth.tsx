c0275b5024554000eb2ff3cf1261102df62153fb
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-07-03 08:45:51 +0100

    start the auth passport

--- src/core/components/MainAuthForm/SocialButtonAuth.tsx
+++ src/core/components/MainAuthForm/SocialButtonAuth.tsx
@@ -1,10 +1,12 @@
-import { Group } from "@mantine/core";
+import { Box, Group } from "@mantine/core";
 import { GoogleButton } from "./Icons/GoogleButton";
 
 const SocialButtonAuth = () => {
   return (
     <Group grow mb="md" mt="md">
-      <GoogleButton radius="xl">Google</GoogleButton>
+      <Box component="a" href="/api/auth/google">
+        <GoogleButton radius="xl">Google</GoogleButton>
+      </Box>
     </Group>
   );
 };
